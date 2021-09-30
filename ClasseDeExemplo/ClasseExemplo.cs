0a512de69c9685080c70cb471eac4c8bc223aa4c
Fred Vieira <fredvieira0709@gmail.com>
2021-09-30 14:17:35 -0300

    Primeiro commit

---
+++ ClasseDeExemplo/ClasseExemplo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClasseDeExemplo
+{
+    public class ClasseExemplo
+    {
+
+        public int somarInteiros(params int[] valores)
+        {
+            int soma = 0;
+
+            for(int i = 0; i < valores.Length; i++)
+            {
+                soma += valores[i];
+            }
+
+            return soma;
+        }
+
+        public string concatenarStrings(string palavra1, string palavra2)
+        {
+            return palavra1 + " " + palavra2;
+        }
+
+        public bool ligado(int num)
+        {
+            bool ligado = false;
+            if(num == 1)
+            {
+                ligado = true;
+            }
+            else
+            {
+                ligado = false;
+            }
+
+            return ligado;
+        }
+    }
+}
---
+++ ClasseDeTestes/ClasseTests.cs
@@ -0,0 +1,42 @@
+﻿using ClasseDeExemplo;
+using NUnit.Framework;
+using System;
+
+namespace ClasseDeTestes
+{
+    [TestFixture]
+    public class ClasseTests
+    {
+        [Test]
+        public void somarNumerosInteiros()
+        {
+            var sut = new ClasseExemplo();
+
+            var resultado = sut.somarInteiros(5, 5, 5, 5);
+
+            Assert.That(resultado, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void juntarEVerificarSeStringExiste()
+        {
+            var sut = new ClasseExemplo();
+
+            var resultado = sut.concatenarStrings("José", "Ferreira");
+
+            StringAssert.Contains("Ferreira", resultado);
+            Assert.That(resultado, Is.EqualTo("José Ferreira"));
+        }
+
+        [Test]
+        public void verificarSeEstaLigado()
+        {
+            var sut = new ClasseExemplo();
+
+            var resultado = sut.ligado(1);
+
+            Assert.True(resultado);
+
+        }
+    }
+}
